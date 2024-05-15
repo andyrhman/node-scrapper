9401d69b31ef45baf5eab0871769f96088bbadcd
andyrhman <andyrahman989@gmail.com>
2024-05-15 19:57:16 +0800

    Modified request using axios instead to download image

--- index.js
+++ index.js
@@ -95,10 +95,11 @@ const urls = [
 
       moviesData.push({ title, date, rating, releaseDate, poster, genres });
 
-      let file = fs.createWriteStream(`./data/image/${movies.id}.jpg`);
-
-      let stream = request({
-        uri: poster,
+      // Download the image
+      const imgResponse = await axios({
+        url: poster,
+        method: "GET",
+        responseType: "stream",
         headers: {
           Accept:
             "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
@@ -121,8 +122,11 @@ const urls = [
           "User-Agent":
             "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36",
         },
-        gzip: true,
-      }).pipe(file);
+      });
+
+      // Pipe the image data to a file
+      const file = fs.createWriteStream(`./data/image/${movies.id}.jpg`);
+      imgResponse.data.pipe(file);
     }
     // Output JSON format
     console.log(JSON.stringify(moviesData, null, 2));
